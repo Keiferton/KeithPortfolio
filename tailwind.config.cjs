347ba64c550762edb400a67fd0302fb00a19fb2a
Keiferton <kcurrysf@gmail.com>
2026-03-29 19:10:11 -0700

    rework visual identity, theme controls, update favicons

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}'],
-  darkMode: 'media',
+  darkMode: 'class',
   theme: {
     extend: {},
   },
